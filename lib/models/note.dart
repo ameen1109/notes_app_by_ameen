97453da9b5156b421614fee412a1183a507f0816
Ameen <ameenathu02@gmail.com>
2023-09-05 19:44:44 +0530

    Initial commit

---
+++ lib/models/note.dart
@@ -0,0 +1,25 @@
+class Note {
+  int id;
+  String title;
+  String content;
+  DateTime modifiedTime;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.modifiedTime,
+  });
+}
+
+
+List<Note> sampleNotes = [
+
+
+
+
+
+
+
+
+];
